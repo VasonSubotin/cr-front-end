9e3ed2b61e4cd53846c3565ced40fd3e3319fc14
Serhii Slobodian <beinc.dn@gmail.com>
2020-06-10 21:29:23 +0300

    login: add login form base

--- src/components/forms/Login/index.jsx
+++ src/components/forms/Login/index.jsx
@@ -1,1 +1,18 @@
-export const LoginForm = () => "Login Form";
+import React from "react";
+import { Grid, Button, TextField } from "@material-ui/core";
+
+export const LoginForm = () => (
+  <Grid component="form" container direction="column" alignItems="center" spacing={2}>
+    <Grid item>
+      <TextField required variant="outlined" label="Email" />
+    </Grid>
+    <Grid item>
+      <TextField required variant="outlined" label="Password" type="password" />
+    </Grid>
+    <Grid item>
+      <Button variant="outlined" color="primary">
+        Sign in
+      </Button>
+    </Grid>
+  </Grid>
+);
