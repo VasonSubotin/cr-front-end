4095591dcafaf5bfd3c180631f5d2c6931a45f12
Serhii Slobodian <beinc.dn@gmail.com>
2020-06-11 16:17:30 +0300

    header: update links

--- src/components/CarsList/index.jsx
+++ src/components/CarsList/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Grid, Typography, Button } from "@material-ui/core";
 import { DirectionsCar } from "@material-ui/icons";
 
-export const CarsList = () => (
+export const CarsList = ({ onAddCarClick }) => (
   <Grid container direction="column" alignItems="center" spacing={2}>
     <Grid item>
       <DirectionsCar fontSize="large" />
@@ -11,9 +12,13 @@ export const CarsList = () => (
       <Typography>You have not added your car yet</Typography>
     </Grid>
     <Grid item>
-      <Button variant="contained" color="primary">
+      <Button onClick={onAddCarClick} variant="contained" color="primary">
         Add a car
       </Button>
     </Grid>
   </Grid>
 );
+
+CarsList.propTypes = {
+  onAddCarClick: PropTypes.func.isRequired,
+};
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,13 +4,15 @@ import { AppBar, Container, Button, Grid } from "@material-ui/core";
 
 import { routes } from "config";
 
+const links = [[routes.MAIN, routes.DEMO, routes.SESSION, routes.SETTINGS], [routes.SIGN_IN]];
+
 /**
  * Website header.
  * Appears in auth state.
  */
 export const Header = () => {
-  const renderLinks = () =>
-    [routes.MAIN, routes.DEMO, routes.SESSION, routes.SETTINGS].map((route) => (
+  const renderLinks = (links) =>
+    links.map((route) => (
       <Grid item key={route.text}>
         <NextLink href={route.href} passHref>
           <Button variant="text">{route.text}</Button>
@@ -21,8 +23,14 @@ export const Header = () => {
   return (
     <AppBar color="inherit" position="fixed">
       <Container>
-        <Grid container wrap="nowrap" justify="center" alignItems="center" spacing={2}>
-          {renderLinks()}
+        <Grid container wrap="nowrap" justify="space-between" alignItems="center" spacing={2}>
+          {links.map((linksGroup, index) => (
+            <Grid key={index} item>
+              <Grid container wrap="nowrap" alignItems="center" spacing={2}>
+                {renderLinks(linksGroup)}
+              </Grid>
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </AppBar>
