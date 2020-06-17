6995f2ce91b2b92dced06cb1dff5b486a67fb727
Serhii Slobodian <beinc.dn@gmail.com>
2020-06-17 19:20:34 +0300

    forgot password: add dialog base

---
+++ src/components/ForgotPasswordDialog/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Dialog, DialogTitle, DialogContent, Grid, Typography } from "@material-ui/core";
+import { RecoverPasswordForm } from "components/forms/RecoverPassword";
+
+export const ForgotPasswordDialog = ({ open, onClose }) => (
+  <Dialog open={open} onClose={onClose}>
+    <DialogTitle>Recover password</DialogTitle>
+    <DialogContent>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography>
+            Please enter the email address for your account. A verification code will be sent to
+            you. Once you have received the verification code, you will be able to choose a new
+            password for your account.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <RecoverPasswordForm />
+        </Grid>
+      </Grid>
+    </DialogContent>
+  </Dialog>
+);
